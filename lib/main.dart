1553ac05d770f460fa3b5810f23aa4c89af4c8d0
shaimaa <shaimaa.a.fakhraldin@gmail.com>
2023-11-07 19:09:16 +0200

    add futur and stream example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:state_example/countState.dart';
 
+import 'future_and_streem.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,12 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: RootWidget()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage());
   }
 }
 
@@ -30,67 +31,62 @@ class RootWidget extends StatefulWidget {
 
 class _RootWidgetState extends State<RootWidget> {
   int count = 0;
-   addCounter(){
-     setState(() {
-       count++;
-     });
-   }
-   removeCount(){
-      setState(() {
-        if(count>0){
-          count--;
-        }
-      });
-   }
+  addCounter() {
+    setState(() {
+      count++;
+    });
+  }
+
+  removeCount() {
+    setState(() {
+      if (count > 0) {
+        count--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return CountState(
-      count:  count,
-      child:  Scaffold(body:
-
-      Column(
+      count: count,
+      child: Scaffold(
+          body: Column(
         children: [
-          SizedBox(height: 50,),
+          SizedBox(
+            height: 50,
+          ),
           InkWell(
-            onTap: (){
-              addCounter();
-            },
-            child:
-
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            color: Colors.red,
-            child: Text("change data"),
-          )),
+              onTap: () {
+                addCounter();
+              },
+              child: Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(20),
+                color: Colors.red,
+                child: Text("change data"),
+              )),
           Expanded(child: MyHomePage(title: 'Flutter Demo Home Page'))
-
         ],
-      )
-      ),
-
-      addCounter:addCounter ,
+      )),
+      addCounter: addCounter,
       removeCounter: removeCount,
-
     );
   }
 }
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
   @override
   Widget build(BuildContext context) {
-    CountState counteSate = context.dependOnInheritedWidgetOfExactType<CountState>()!;
+    CountState counteSate =
+        context.dependOnInheritedWidgetOfExactType<CountState>()!;
     return Scaffold(
       appBar: AppBar(
-
         title: Text(title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -103,6 +99,6 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
-     );
+    );
   }
 }
